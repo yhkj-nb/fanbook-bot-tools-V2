d85bf5f18cdac761e9e67a5baec3ce9d755b029e
云痕科技 <lianxi_0917zhihao@qq.com>
2026-09-19 14:24:18 +0800

    feat(footer): 仓库/项目链接归组并以连接符分隔，保留关于入口 (yhkj-nb)

--- layouts/footer.vue
+++ layouts/footer.vue
@@ -8,18 +8,21 @@ export interface FooterItem {
   label: string;
   link: string;
 }
-const data: FooterItem[] = [{
+/** 社区相关链接。 */
+const communityLinks: FooterItem[] = [{
   label: 'Fanbook 开放平台',
   link: 'https://open.fanbook.mobi/',
 }, {
+  label: 'QQ 群',
+  link: 'https://yhkj-nb.github.io/QQ.html',
+}];
+/** 仓库 / 项目相关链接（用连接符归在一起）。 */
+const repositoryLinks: FooterItem[] = [{
   label: '原仓库',
   link: 'https://github.com/Starlight-Dev-Team/fanbook-bot-tools',
 }, {
   label: '由 yhkj-nb 修改',
   link: 'https://github.com/yhkj-nb/fanbook-bot-tools-V2',
-}, {
-  label: 'QQ 群',
-  link: 'https://yhkj-nb.github.io/QQ.html',
 }, {
   label: '关于',
   link: '/about',
@@ -30,14 +33,25 @@ const data: FooterItem[] = [{
 </script>
 
 <template>
-  <TypographyText>
-    <TypographyText v-for='(item, index) in data'>
-      <Divider v-if='!!index' direction='vertical' />
-      <AppLink :to='item.link'>
-        {{ item.label }}
-      </AppLink>
+  <div class='footer-links'>
+    <TypographyText>
+      <TypographyText v-for='(item, index) in communityLinks'>
+        <Divider v-if='!!index' direction='vertical' />
+        <AppLink :to='item.link'>
+          {{ item.label }}
+        </AppLink>
+      </TypographyText>
     </TypographyText>
-  </TypographyText>
+    <Divider direction='horizontal' />
+    <TypographyText>
+      <TypographyText v-for='(item, index) in repositoryLinks'>
+        <Divider v-if='!!index' direction='vertical' />
+        <AppLink :to='item.link'>
+          {{ item.label }}
+        </AppLink>
+      </TypographyText>
+    </TypographyText>
+  </div>
   <TypographyText>
     Released under
     <AppLink to='https://opensource.org/license/mit/' :hoverable='false'>
@@ -54,5 +68,8 @@ const data: FooterItem[] = [{
 <style scoped>
 span.arco-typography:deep() {
   text-align: center;
+}
+.footer-links {
+  margin-bottom: 8px;
 }
 </style>
