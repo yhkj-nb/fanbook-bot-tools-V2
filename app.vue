d1a565ca664d770ff143b46a66329e0661889a6d
云痕科技 <lianxi_0917zhihao@qq.com>
2026-09-24 19:20:15 +0800

    feat: WinUI3 主题 + 用户名搜索用户 + @云痕科技 水印 (yhkj-nb)

--- app.vue
+++ app.vue
@@ -14,6 +14,7 @@ checkAuth(useRoute());
   <Teleport to='head'>
     <title>Fanbook 机器人工具</title>
   </Teleport>
+  <div class='brand-watermark'>@云痕科技</div>
   <LayoutHeader>
     <NuxtLayout name='header' />
   </LayoutHeader>
